cdd4ff5dd1e53c44ff0ae1c86544de95bb4a4bf7
Ivan Esmantovich <ivanesmantovich@gmail.com>
2023-01-26 20:31:32 +0300

    first commit

---
+++ 412.ts
@@ -0,0 +1,20 @@
+function getFizzBuzzValue(num: number): string {
+    if (num % 3 === 0 && num % 5 === 0) {
+        return 'FizzBuzz';
+    }
+    if (num % 3 === 0) {
+        return 'Fizz';
+    }
+    if (num % 5 === 0) {
+        return 'Buzz';
+    }
+    return num.toString();
+}
+
+function fizzBuzz(n: number): string[] {
+    let result: string[] = [];
+    for (let currentNumber = 1; currentNumber <= n; currentNumber++) {
+        result.push(getFizzBuzzValue(currentNumber));
+    }
+    return result;
+}
