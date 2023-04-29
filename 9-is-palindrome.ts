d8a9a9854d9eedbb8652344b756b216a16a1d8ec
Ivan Esmantovich <ivanesmantovich@gmail.com>
2023-04-29 17:04:20 +0300

    9-is-palindrome

--- 9-is-palindrome.ts
+++ 9-is-palindrome.ts
@@ -1,9 +1,39 @@
 // Palindrome number (https://leetcode.com/problems/palindrome-number/)
 
-// Simple solution
-function isPalindrome(x: number): boolean {
-    return x.toString() === x.toString().split('').reverse().join('')
-}
+// Простое решение
+// function isPalindrome(x: number): boolean {
+//     return x.toString() === x.toString().split('').reverse().join('')
+// }
 
-// Optimal solution (https://www.youtube.com/watch?v=yubRKwixN-U)
-// TODO
+// Оптимальное решение (https://www.youtube.com/watch?v=yubRKwixN-U)
+function isPalindrome(x: number): boolean {
+    if (x < 0) return false
+
+    // 1. Нужен делитель состоящий из единицы и нулей и равный по длине числу
+    let div = 1
+    // 2. Начинаем с единицы и добавляем нули умножениями на 10
+    while (x >= div * 10) div *= 10
+
+    // 3. Цикл пока число не равно нулю
+    while (x) {
+        // 4. Получаем ТОЛЬКО левую цифру разделив на делитель
+        const leftNum = Math.floor(x / div)
+        // 5. Получаем ТОЛЬКО правую цифру взяв остаток от деления на 10
+        const rightNum = x % 10
+        // 6. Если цифры не равны возвращаем false
+        if (leftNum !== rightNum) return false
+
+        // Для отрезания просто разворачиваем операции
+        // 7. Отрезаем левую цифру числа, взяв остаток от деления на делитель
+        // 1234 -> 234        1234 / 1000    234
+        x = x % div
+        // 8. Отрезаем правую цифру числа, разделив на 10
+        // 234 -> 23         234 / 10       23
+        x = Math.floor(x / 10)
+        // 9. Отрезаем две правые цифры делителя, разделив на 100
+        // так как убрали две цифры у числа
+        div = Math.floor(div / 100)
+    }
+    // 10. Если цикл завершен, то возвращаем true
+    return true
+}
